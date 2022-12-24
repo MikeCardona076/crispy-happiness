a752aa58b8531dae140b3ab574035a14f2f59668
Mike Cardona <40007337+MikeCardona076@users.noreply.github.com>
2022-12-24 14:56:04 -0800

    Test

--- enviar_mensaje_masivo.py
+++ enviar_mensaje_masivo.py
@@ -1,25 +1,20 @@
 "INSTALA LA LIBRERIA pip install pywhatkit "
 
-
 import pywhatkit
 
-pywhatkit.start_server()
+# Iniciar el servidor
+# pywhatkit.start_server()
 
+# Crear una lista de números de teléfono
+numeros = ['+52 1 55 1186 9158']
 
-lista_contactos = [
-
-   '+5215511869158'
-]
-
-
-#Envia el Mensaje 
-
-for i in lista_contactos:
-    
-    pywhatkit.sendwhatmsg_instantly(i, """Holaaaaaaaaa. ¡Feliz Navidad! Espero que estés pasando unas fiestas maravillosas rodeado de tu familia y amigos.
+# Enviar mensaje a cada número de teléfono
+for numero in numeros:
+    pywhatkit.sendwhatmsg_instantly(numero, """Holaaaaaaaaa. ¡Feliz Navidad! Espero que estés pasando unas fiestas maravillosas rodeado de tu familia y amigos.
 
     Quiero aprovechar esta ocasión para expresar mis mejores deseos hacia ti y para desearte un año lleno de éxitos y felicidad. Espero que puedas disfrutar al máximo de estas fiestas y que puedas relajarte y descansar junto a tu familia.
 
     La Navidad es un tiempo para celebrar y reflexionar sobre lo que hemos logrado durante el año. Estoy seguro de que has tenido muchos triunfos y logros que celebrar, y espero que puedas seguir trabajando duro y alcanzando tus metas en el año que viene.
 
-    Ojalá puedas disfrutar de esta época del año con los tuyos y puedas compartir momentos especiales y alegres con ellos. Que tengas una maravillosa Navidad y un próspero Año Nuevo. ¡Te deseo todo lo mejor!""", 4, False, 4)
+    Ojalá puedas disfrutar de esta época del año con los tuyos y puedas compartir momentos especiales y alegres con ellos. Que tengas una maravillosa Navidad y un próspero Año Nuevo. ¡Te deseo todo lo mejor!""")
+
